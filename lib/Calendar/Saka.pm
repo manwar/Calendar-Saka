f39107d247390da72128e488db84c14851cd9957
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2016-12-10 12:37:24 +0000

    - Added test scripts (t/meta-json.t and t/meta-yml.t).

--- lib/Calendar/Saka.pm
+++ lib/Calendar/Saka.pm
@@ -1,6 +1,6 @@
 package Calendar::Saka;
 
-$Calendar::Saka::VERSION   = '1.30';
+$Calendar::Saka::VERSION   = '1.31';
 $Calendar::Saka::AUTHORITY = 'cpan:MANWAR';
 
 =head1 NAME
@@ -9,7 +9,7 @@ Calendar::Saka - Interface to Indian Calendar.
 
 =head1 VERSION
 
-Version 1.30
+Version 1.31
 
 =cut
 
